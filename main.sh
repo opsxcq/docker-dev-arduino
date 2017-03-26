9c282271d09a49bdc1edf00443491729fd0cdde0
OPSXCQ <opsxcq@strm.sh>
2017-03-25 23:42:06 -0300

    fix

--- main.sh
+++ main.sh
@@ -4,12 +4,16 @@ if [ -z "$1" ]
 then
     bash
 elif [ "$1" = "init" ]
+then
     ino init
 elif [ "$1" = "build" ]
+then
     ino build
 elif [ "$1" = "upload" ]
+then
     ino upload
 elif [ "$1" = "serial" ]
+then
     ino serial
 else
     echo '[-] Command not found '$1
