b003fa389b15e1b7ae5b67666530e86c9d43860b
OPSXCQ <opsxcq@strm.sh>
2017-03-25 23:34:51 -0300

    update

--- main.sh
+++ main.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-bash
+if [ -z "$1" ]
+then
+    bash
+elif [ "$1" = "init" ]
+    ino init
+elif [ "$1" = "build" ]
+    ino build
+elif [ "$1" = "upload" ]
+    ino upload
+elif [ "$1" = "serial" ]
+    ino serial
+elif
+    echo '[-] Command not found '$1
+    exit -1
+fi
